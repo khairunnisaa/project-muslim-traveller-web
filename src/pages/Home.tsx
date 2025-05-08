33ffc35e6cc7dd257231dd5e34770feaa54a7507
khairunnisaa <nissa.khairunnisaaa@gmail.com>
2025-05-08 22:27:20 +0800

    update card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {
+    Box,
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Divider,
+    Grid, IconButton,
+    Typography
+} from "@mui/material";
 import {MuiColor} from "../styles/enum";
 import services1 from "../assets/image/services-1.jpg";
 import services2 from "../assets/image/services-2.jpg";
 import services3 from "../assets/image/services-3.jpg";
 import services4 from "../assets/image/services-4.jpg";
+import background_1 from "../assets/image/background_1.png";
+import Chip from "@mui/material/Chip";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import "../styles/listDetinatiom.css"
+import CardGrid from "../component/CardGrid/CardGrid";
+import data from "../data/dummy.json"
+
 const Home = () => (
 
     <Box mt={9}>
@@ -113,10 +131,290 @@ const Home = () => (
                     a large language ocean.
                     A small river named Duden flows by their place and supplies it with the necessary regelialia.
                 </Typography>
-
-
+                <Button size={"large"} variant={"contained"} sx={{bgcolor: MuiColor.Orange, marginTop: 5}}>Search Destination</Button>
             </Grid>
         </Grid>
+
+
+        <Box mt={10} pb={20} sx={{backgroundImage:`url(${background_1})`, backgroundSize: "cover", }}>
+            <Box p={2} sx={{flexDirection: 'row', textAlign: 'center' }}>
+                <Typography sx={{
+                    fontSize: {xs: 25, md: 30},
+                    color: MuiColor.Orange,
+                    fontWeight: 700,
+                    fontFamily: "Arizonia, cursive",
+                }}>
+                    Pacific Provide Places
+                </Typography>
+
+                <Typography variant="h1" component="p" sx={{
+                    fontSize: {xs: 25, md: 50},
+                    lineHeight: 1.2,
+                    fontWeight: 700,
+                }}>
+                    Select Your Destination
+                </Typography>
+
+            </Box>
+            <Grid marginTop={5} container={true} spacing={2} >
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card className="card" sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                               Indonesia
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid size={{xs:12, sm: 4, md: 3}}>
+                    <Card sx={{ maxWidth: 400}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={services1}
+                                alt="green iguana"
+                                sx={{maxHeight: 400, maxWidth: 400}}
+                            />
+                            <CardActions sx={{ color: MuiColor.Orange, fontSize: "16px" }}>
+                                <Typography variant="body2">
+                                    Canada
+                                </Typography>
+                                <Divider orientation="vertical" flexItem></Divider>
+                                <Typography variant="subtitle2">
+                                    7 Tour
+                                </Typography>
+                            </CardActions>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+
+
+        <Box mt={10} pb={20}>
+            <Box p={2} sx={{flexDirection: 'row', textAlign: 'center' }}>
+                <Typography sx={{
+                    fontSize: {xs: 25, md: 30},
+                    color: MuiColor.Orange,
+                    fontWeight: 700,
+                    fontFamily: "Arizonia, cursive",
+                }}>
+                    Destination
+                </Typography>
+
+                <Typography variant="h1" component="p" sx={{
+                    fontSize: {xs: 25, md: 50},
+                    lineHeight: 1.2,
+                    fontWeight: 700,
+                }}>
+                    Tour Destination
+                </Typography>
+            </Box>
+
+            <Grid container>
+                <Grid size={{xs:10}} display="flex">
+                    <Card sx={{ display: "flex" }} className="card">
+                        <div className="cardImage">
+                            <CardMedia
+                                component="img"
+                                alt="auction image"
+                                image="https://picsum.photos/536/354"
+                            />
+                        </div>
+                        <div className="details">
+                            <div className="content">
+                                <CardContent>
+                                    <Typography variant="h5" color="primary" id="auctionTitle">
+                                        Test auction
+                                    </Typography>
+                                    <Typography variant="body2" component="p" gutterBottom>
+                                        DECEMBER 22, 2022 - DECEMBER 22, 2022 | Published
+                                    </Typography>
+                                    <Typography variant="body2" component="p" color="textSecondary">
+                                        Ok !
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                            <div className="controls">
+                                <Button>Edit</Button>
+                                <Divider orientation="vertical" flexItem />
+                                <Button>Lots</Button>
+                                <Divider orientation="vertical" flexItem />
+                                <Button>import lots</Button>
+                                <Divider orientation="vertical" flexItem />
+                                <Button>view reports</Button>
+                            </div>
+                        </div>
+                        <div className="delete">
+                            <Chip label="LIVE" color="error" />
+                            <IconButton aria-label="settings">
+                                <DeleteOutlineOutlinedIcon color="error" />
+                            </IconButton>
+                        </div>
+                    </Card>
+                </Grid>
+            </Grid>
+
+            <Grid
+                container
+                spacing={4}
+                justifyContent="center"
+                display="flex"
+                style={{ padding: "16px" }}
+            >
+                <Grid size={{xs:12}}>
+                    <Grid
+                        container
+                        spacing={4}
+                        justifyContent="center"
+                        display="flex"
+                        style={{ padding: "16px" }}
+                    >
+                        {data?.auctionDetails?.map((auction) => (
+                            <CardGrid auction={auction} />
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
     </Box>
 
 
